8dd767372f986dc7904dc4c69e170885ffe3fbcf
kestrelsmotorcompany-boop <kestrelsmotorcompany@gmail.com>
2026-08-14 08:56:28 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='my-kestrels-v1';
+const CACHE='my-kestrels-v2';
 const ASSETS=['/','/styles.css','/app.js','/manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
