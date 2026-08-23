d1380a39e184c022da4d1150a0aebc1a262f2808
kestrelsmotorcompany-boop <kestrelsmotorcompany@gmail.com>
2026-08-23 21:30:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const pool = new Pool({
 });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 async function setupDatabase() {
   await pool.query(`
